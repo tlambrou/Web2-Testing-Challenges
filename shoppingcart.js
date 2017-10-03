93c169373ac151252a359e5ba62f926adbc746f5
tlambrou <lambrou@gmail.com>
2017-10-03 15:17:48 -0700

    Got 3 tests working

--- shoppingcart.js
+++ shoppingcart.js
@@ -1,1 +1,59 @@
 // Shopping cart functionality
+
+
+module.exports.ShoppingCart = class ShoppingCart {
+
+  constructor() {
+    // Initialize items array
+    this.items = []
+    // // Add the quantity property to the item object
+    // item['quantity'] = 1
+    // // Add the initial item to the cart
+    // this.items.append(item)
+  }
+
+  addItem(item) {
+
+    //Go through each item in the cart
+    this.items.map((i) => {
+      // Check and see if the specified item is already in the shopping cart
+      if (item.name in i) {
+        // If item in cart, then simply increase the quanitty by one
+        i.quantity++
+        return i
+      // Otherwise add the item to the shopping cart
+      } else {
+        // Add the quantity property to the item object
+        item['quantity'] = 1
+        // Add the item to the cart
+        this.items.append(item)
+      }
+    })
+  }
+
+  remove(itemName) {
+    var contained = false
+    // Go through each item in the cart
+    this.item.map((i, index) => {
+      if (i.name == itemName && i.quantity == 1) {
+        this.item.splice(index, 1)
+        contained = true
+      } else if (i.name == itemName && i.quanity > 1) {
+        i.quantity--
+      }
+    })
+
+    if (contained == false) {
+      console.log("Nothing to remove")
+    }
+
+  }
+
+  totalCost() {
+    return this.item.reduce((acc, item) => {
+      acc += (item.price * item.quantity)
+    })
+  }
+
+
+}
--- shoppingcart.test.js
+++ shoppingcart.test.js
@@ -1,17 +1,21 @@
 // Tests for the shpping cart module
 
-const chai = require('chai')
+var ShoppingCart = require('./shoppingcart').ShoppingCart
 
-const shoppingcart = require('./shoppingcart')
+const chai = require('chai')
+const should = require('should');
 
 it('Should create a new item with name and price', () => {
   const apple = {
     price: 0.89,
     name: "apple"
   }
-  var cart = new ShoppingCart(apple)
-  cart.should.have.property('name')
-  cart.should.have.property('price')
+  var cart = new ShoppingCart()
+  cart.addItem(apple)
+
+  console.log(cart.items)
+  should(cart.items[0]).have.property('name')
+  should(cart.items[0]).have.property('price')
 });
 
 it('Should add a new item to the shopping cart', () => {
@@ -68,19 +72,84 @@ it('Should return an array containing all items in cart', () => {
 
 it('Should remove items from cart', () => {
 
+  const apple = { price: 0.89, name: "apple" }
+  const brainz = { price: 852.71, name: "brainz" }
+  const morty = { price: 99.99, name: "morty" }
+  const rick = { price: 99999999999.99, name: "rick" }
 
+  var cart = new ShoppingCart()
+
+  cart.addItem(apple)
+  cart.addItem(brainz)
+  cart.addItem(rick)
+  cart.addItem(morty)
+
+  //Tests
+  cart.items.should.have.property('rick')
+
+  cart.remove('rick')
+
+  cart.items.should.not.have.property('rick')
 
 });
 
 // Stretch challenges
 it('Should update the count of items in the cart', () => {
 
+  const morty = { price: 99.99, name: "morty" }
+  const rick = { price: 99999999999.99, name: "rick" }
+
+  var cart = new ShoppingCart()
+
+  cart.count.should.be.equal(0)
+
+  cart.addItem(morty)
+
+  cart.count.should.be.equal(1)
+
+  cart.addItem(rick)
+
+  cart.count.should.be.equal(2)
+
+  cart.addItem(morty)
+
+  cart.count.should.be.equal(3)
+
+  cart.remove('morty')
+
+  cart.count.should.be.equal(2)
+
 });
 
 it('Should remove an item when its count is 0', () => {
 
+  const rick = { price: 99999999999.99, name: "rick" }
+
+  var cart = new ShoppingCart()
+
+  cart.addItem(rick)
+
+  cart.count.should.be.equal(1)
+
+  cart.remove('rick')
+
+  cart.items.length.should.be.equal(0)
+
 });
 
 it('Should return the total cost of all items in the cart', () => {
-  
+
+  const morty = { price: 99.99, name: "morty" }
+  const rick = { price: 999999.99, name: "rick" }
+
+  var cart = new ShoppingCart()
+  cart.addItem(rick)
+  cart.addItem(morty)
+
+  cart.totalCost().should.be.equal(1000099.98)
+
+  cart.addItem(morty)
+
+  cart.totalCost().should.be.equal(1000199.97)
+
 });
